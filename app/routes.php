69b6a64c6421927d58c81f7af3b3adf07d063268
Jia Miew (John) Tan <john@binarix.net>
2013-05-23 10:33:44 +1000

    added vardumps

--- app/routes.php
+++ app/routes.php
@@ -19,11 +19,16 @@ Route::get('/', function()
 Route::get('/get', function()
 {
 	$followings = User::find(2)->followings()->with('userprofile')->get();
+    var_dump($followings->toArray());
     return View::make('followings', compact('followings'));
 });
 
 Route::get('/paginate', function()
 {
     $followings = User::find(2)->followings()->with('userprofile')->paginate(1);
+    foreach ($followings as $following)
+    {
+        var_dump($following->toArray());
+    }
     return View::make('followingspaginate', compact('followings'));
 });

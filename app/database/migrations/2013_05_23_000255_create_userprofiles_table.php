4bbbf66d6f565b409a07f8284690034e7f224ae6
Jia Miew (John) Tan <john@binarix.net>
2013-06-02 13:28:20 +1000

    should not have removed the migration

---
+++ app/database/migrations/2013_05_23_000255_create_userprofiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserprofilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('userprofiles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned()->default(0);
+            $table->string('name')->default('');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('userprofiles');
+	}
+
+}
